7776161fb94462b29e5bc0acc65aef7d3374a9d3
Matias Meno <m@tias.me>
2021-03-18 17:58:42 +0100

    Show how jest-dom should be used

--- src/components/__tests__/Title.test.tsx
+++ src/components/__tests__/Title.test.tsx
@@ -1,3 +1,4 @@
+import "@testing-library/jest-dom"; // Provides jest dom matchers
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import Title from "../Title";
